94602459e3f9d6f466eb24e8142e08c366cc36dd
Oliver Thomas <olliethomas86@gmail.com>
2017-02-01 17:19:18 +0000

    ?/
    ////

--- mla/svm/svm.py
+++ mla/svm/svm.py
@@ -1,5 +1,5 @@
-from mla.base import BaseEstimator
-from mla.svm.kernerls import Linear
+#from mla.base import BaseEstimator
+#from mla.svm.kernerls import Linear
 import numpy as np
 import logging
 
@@ -9,6 +9,68 @@ np.random.seed(9999)
 References:
 The Simplified SMO Algorithm http://cs229.stanford.edu/materials/smo.pdf
 """
+
+class Linear(object):
+    def __call__(self, x, y):
+        return np.dot(x, y.T)
+    def __repr__(self):
+        return 'Linear kernel'
+
+class BaseEstimator(object):
+    X = None
+    y = None
+    y_required = True
+
+    def _setup_input(self, X, y=None):
+        """Ensure inputs to an estimator are in the expected format.
+        Ensures X and y are stored as numpy ndarrays by converting from an
+        array-like object if necessary. Enables estimators to define whether
+        they require a set of y target values or not with y_required, e.g.
+        kmeans clustering requires no target labels and is fit against only X.
+        Parameters
+        ----------
+        X : array-like
+            Feature dataset.
+        y : array-like
+            Target values. By default is required, but if y_required = false
+            then may be omitted.
+        """
+        if not isinstance(X, np.ndarray):
+            X = np.array(X)
+
+        if X.size == 0:
+            raise ValueError('Number of features must be > 0')
+
+        if X.ndim == 1:
+            self.n_samples, self.n_features = 1, X.shape
+        else:
+            self.n_samples, self.n_features = X.shape[0], np.prod(X.shape[1:])
+
+        self.X = X
+
+        if self.y_required:
+            if y is None:
+                raise ValueError('Missed required argument y')
+
+            if not isinstance(y, np.ndarray):
+                y = np.array(y)
+
+            if y.size == 0:
+                raise ValueError('Number of features must be > 0')
+
+        self.y = y
+
+    def fit(self, X, y=None):
+        self._setup_input(X, y)
+
+    def predict(self, X=None):
+        if self.X is not None:
+            return self._predict(X)
+        else:
+            raise ValueError('You must call `fit` before `predict`')
+
+    def _predict(self, X=None):
+        raise NotImplementedError()
 
 
 class SVM(BaseEstimator):
@@ -54,6 +116,8 @@ class SVM(BaseEstimator):
                 i = self.random_index(j)
 
                 eta = 2.0 * self.K[i, j] - self.K[i, i] - self.K[j, j]
+                print("i = ", i, "j = ", j)
+                print("eta = ", eta, "self.K[i, j] then ii and ij = ", self.K[i, j], self.K[i, i], self.K[j, j])
                 if eta >= 0:
                     continue
                 L, H = self._find_bounds(i, j)
@@ -71,16 +135,20 @@ class SVM(BaseEstimator):
                 self.alpha[i] = self.alpha[i] + self.y[i] * self.y[j] * (alpha_jo - self.alpha[j])
 
                 # Find intercept
+                print("b = ", self.b, " self.y[i] = ", self.y[i], "alpha[i]", self.alpha[i])
                 b1 = self.b - e_i - self.y[i] * (self.alpha[i] - alpha_jo) * self.K[i, i] - \
                      self.y[j] * (self.alpha[j] - alpha_jo) * self.K[i, j]
+                print("b1 = ", b1)
                 b2 = self.b - e_j - self.y[j] * (self.alpha[j] - alpha_jo) * self.K[j, j] - \
                      self.y[i] * (self.alpha[i] - alpha_io) * self.K[i, j]
+                print("b2 = ", b2)
                 if 0 < self.alpha[i] < self.C:
                     self.b = b1
                 elif 0 < self.alpha[j] < self.C:
                     self.b = b2
                 else:
                     self.b = 0.5 * (b1 + b2)
+                print("self.b = ", self.b)
 
             # Check convergence
             diff = np.linalg.norm(self.alpha - alpha_prev)
@@ -90,6 +158,9 @@ class SVM(BaseEstimator):
 
         # Save support vectors index
         self.sv_idx = np.where(self.alpha > 0)[0]
+        print("w = ", self.w)
+        print("b = ", self.b)
+        print("alphas = ", self.alpha)
 
     def _predict(self, X=None):
         n = X.shape[0]
@@ -99,6 +170,7 @@ class SVM(BaseEstimator):
         return result
 
     def _predict_row(self, X):
+        self.w = np.dot((self.alpha * self.y), self.X)
         k_v = self.kernel(self.X[self.sv_idx], X)
         return np.dot((self.alpha[self.sv_idx] * self.y[self.sv_idx]).T, k_v.T) + self.b
 
@@ -130,3 +202,9 @@ class SVM(BaseEstimator):
         while i == z:
             i = np.random.randint(0, self.n_samples - 1)
         return i
+
+s = SVM()
+s.fit([[-1,-4],[-2,-3],[-1,-1],[-3,-2],[2,1],[1,1],[1,2]], [-1,-1,-1,-1,1,1,1])
+x = np.array([[-2,-2]])
+print(s.predict(x), s.alpha, s.b)
+print(s.w)
